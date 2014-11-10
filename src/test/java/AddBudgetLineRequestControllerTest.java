cbf368cd9472b637dea8e839806d875ae7ae2888
DEG <jogalt@gmail.com>
2014-11-10 15:22:42 +0100

    Added junit hierarchicalContextRunner, just to try it Split up unit tests into two parts

--- src/test/java/AddBudgetLineRequestControllerTest.java
+++ src/test/java/AddBudgetLineRequestControllerTest.java
@@ -1,63 +1,86 @@
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by dino on 07/11/14.
  */
+@RunWith(HierarchicalContextRunner.class)
 public class AddBudgetLineRequestControllerTest {
+
+
+    public static final double ACCURACY = 0.00001;
+    private static final int id = 1;
+    private static final String categoryName = "CategoryName";
+    private static final String budgetLineName = "BudgetLineName";
+    private static final double budget = 2000;
 
     private RequestBuilder requestBuilder;
     private UseCaseFactory useCaseFactory;
 
-    public static final double ACCURACY = 0.00001;
+    public class MonthlyBudgetContext {
+        @Before
+        public void setUp() throws Exception {
+            requestBuilder = new AddBudgetLineBuilder();
+            useCaseFactory = new AddMonthlyBudgetLineFactory();
+        }
 
-    private final int id = 1;
-    private final String categoryName = "CategoryName";
-    private final String budgetLineName = "BudgetLineName";
-    private final double budget = 2000;
+        @Test
+        public void addedBudgetLine_canBeRetrieved() {
+            addBudgetLine();
 
+            BudgetLine budgetLine = loadBudgetLine();
+            validateBudgetContent(budgetLine);
 
-    @Before
-    public void setUp() throws Exception {
-        requestBuilder = new AddBudgetLineBuilder();
-        useCaseFactory = new AddMonthlyBudgetLineFactory();
+            BudgetLineFrequency frequency = budgetLine.getBudgetLineFrequency();
+            assertThat(frequency, instanceOf(BudgetLineMonthlyFrequency.class));
+            assertEquals(budget, frequency.getMonthlyBudget(), ACCURACY);
+        }
     }
 
-    @Test
-    public void addMonthlyBudgetLine() {
 
+    public class YearlyBudgetContext {
+        @Before
+        public void setUp() throws Exception {
+            requestBuilder = new AddBudgetLineBuilder();
+            useCaseFactory = new AddYearlyBudgetLineFactory();
+        }
 
-        addBudgetLine();
+        @Test
+        public void addedBudgetLine_canBeRetrieved() {
 
-        BudgetLine budgetLine = BudgetDataBase.budgetDataBase.getBudgetLine(id);
+            addBudgetLine();
+
+            BudgetLine budgetLine = loadBudgetLine();
+            validateBudgetContent(budgetLine);
+
+            BudgetLineFrequency frequency = budgetLine.getBudgetLineFrequency();
+            assertThat(frequency, instanceOf(BudgetLineYearlyFrequency.class));
+
+            assertEquals(budgetLineName, budgetLine.getBudgetLineName());
+            assertEquals(budget / 12, frequency.getMonthlyBudget(), ACCURACY);
+        }
+
+    }
+
+    private void validateBudgetContent(BudgetLine budgetLine) {
         assertEquals(categoryName, budgetLine.getCategoryName());
         assertEquals(budgetLineName, budgetLine.getBudgetLineName());
-
-        BudgetLineFrequency frequency = budgetLine.getBudgetLineFrequency();
-        assertThat(frequency, instanceOf(BudgetLineMonthlyFrequency.class));
-        assertEquals(budget, frequency.getMonthlyBudget(), ACCURACY);
     }
 
-    @Test
-    public void addYearlyBudgetLine() {
-
-        addBudgetLine();
-
-        BudgetLine budgetLine = BudgetDataBase.budgetDataBase.getBudgetLine(id);
-        assertEquals(categoryName, budgetLine.getCategoryName());
-        assertEquals(budgetLineName, budgetLine.getBudgetLineName());
-
-        BudgetLineFrequency frequency = budgetLine.getBudgetLineFrequency();
-        assertThat(frequency, instanceOf(BudgetLineYearlyFrequency.class));
-
-        assertEquals(budgetLineName, budgetLine.getBudgetLineName());
-        assertEquals(budget / 12, frequency.getMonthlyBudget(), ACCURACY);
+    private BudgetLine loadBudgetLine() {
+        return BudgetDataBase.budgetDataBase.getBudgetLine(AddBudgetLineRequestControllerTest.id);
     }
+
 
     private void addBudgetLine() {
-        Request request = requestBuilder.createAddBudgetLineRequest(id, categoryName, budgetLineName, budget);
+        Request addBudgetLineRequest = requestBuilder.createAddBudgetLineRequest(id, categoryName, budgetLineName, budget);
         UseCase useCase = useCaseFactory.CreateUseCase();
-        useCase.Execute(request);
+        useCase.Execute(addBudgetLineRequest);
     }
 }
