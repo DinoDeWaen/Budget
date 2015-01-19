acc7656798e4e3612ecde3dabd392ca9449a5574
Dino <dino@cerm.be>
2015-01-19 14:31:51 +0100

    added getters for totals, budget and cashflows

--- src/main/java/balance/Balance.java
+++ src/main/java/balance/Balance.java
@@ -2,4 +2,17 @@ package balance;
 
 public class Balance {
 
+	public double getTotalBudgetIncome() {
+		return 2000.00;
+	}
+	public double getTotalBudgetExpense() {
+		return 1000.00;
+	}
+	public double getTotalIncome() {
+		return 1582.55;
+	}
+	public double getTotalExpense() {
+		return 685.57;
+	}
+
 }
--- src/test/java/BalanceTest.java
+++ src/test/java/BalanceTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import balance.Balance;
@@ -6,10 +8,15 @@ import balance.Balance;
 public class BalanceTest {
 	
 	Balance balance; 
+	private static final double ACCURACY = 0.00001;
 	
 	@Test
 	public void createBalanceTest(){
-		balance = new Balance ();		
+		balance = new Balance ();
+		assertEquals(balance.getTotalBudgetIncome(), 2000.00, ACCURACY);
+		assertEquals(balance.getTotalBudgetExpense(), 1000.00, ACCURACY); 
+		assertEquals(balance.getTotalIncome(), 1582.55, ACCURACY);
+		assertEquals(balance.getTotalExpense(), 685.57, ACCURACY); 		
 	}
 
 }
