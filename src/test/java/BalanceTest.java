937268c18aa95e242bd9cb8cba1c164fed73d880
Dino <dino@cerm.be>
2015-01-19 14:01:23 +0100

    Create balance test cleared

--- src/test/java/BalanceTest.java
+++ src/test/java/BalanceTest.java
@@ -1,4 +1,15 @@
+import org.junit.Test;
+
+import balance.Balance;
+
 
 public class BalanceTest {
+	
+	Balance balance; 
+	
+	@Test
+	public void createBalanceTest(){
+		balance = new Balance ();		
+	}
 
 }
