223eec1b1af38cbfdf7b47a6ad8bee5c7b80f451
Dino <dinodewaen@gmail.com>
2015-01-30 16:05:17 +0100

    Extra tests for the gateway

--- src/test/java/gateway/gatewayTest.java
+++ src/test/java/gateway/gatewayTest.java
@@ -9,16 +9,30 @@ import category.Category;
 public class gatewayTest {
 
 	private Integer catId1, catId2, catId3;
-	
+	private final String TEST1 = "test1";
+	private final String TEST2 = "test2";
+	private final String TEST3 = "test3";
+		
 	@Test
 	public void test() {
 		BudgetDataBase.load();	
-		catId1 = BudgetDataBase.getDB().addCategory(Category.newBuilder().withCategoryName("test").build());
+		catId1 = BudgetDataBase.getDB().addCategory(Category.newBuilder().withCategoryName(TEST1).build());
 		BudgetDataBase.save();
-		catId2 = BudgetDataBase.getDB().addCategory(Category.newBuilder().withCategoryName("test2").build());
+		catId2 = BudgetDataBase.getDB().addCategory(Category.newBuilder().withCategoryName(TEST2).build());
 		BudgetDataBase.load();	
-		assertEquals(BudgetDataBase.getDB().getCategory(catId1).getName(), "test");
+
+		assertEquals(BudgetDataBase.getDB().getCategory(catId1).getName(), TEST1);
 		assertNull(BudgetDataBase.getDB().getCategory(catId2));
+		
+		catId2 = BudgetDataBase.getDB().addCategory(Category.newBuilder().withCategoryName(TEST2).build());
+		BudgetDataBase.save();
+		catId3 = BudgetDataBase.getDB().addCategory(Category.newBuilder().withCategoryName(TEST3).build());
+		BudgetDataBase.load();
+		
+
+		assertEquals(BudgetDataBase.getDB().getCategory(catId1).getName(), TEST1);		
+		assertEquals(BudgetDataBase.getDB().getCategory(catId2).getName(), TEST2);
+		assertNull(BudgetDataBase.getDB().getCategory(catId3));
 	}
 
 }
